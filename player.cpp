e25baad53bacddaaf983f71c275fc7a41b2ab8a6
Mateusz Fecowicz <mfecowicz0@gmail.com>
2025-10-20 23:10:05 +0200

    Remove old files

--- player.cpp
+++
@@ -1,5 +0,0 @@
-#include "player.h"
-
-
-Player::Player(int start_amount) : money(start_amount) {}
-
--- save.cpp
+++
@@ -1,3 +0,0 @@
-#include "save.h"
-
-SaveFile::SaveFile(std::string filename) : FileManager(filename) {}
